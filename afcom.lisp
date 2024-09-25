c496945529d01c8c12b6e4caf652b0b41dcffe8b
Omar Antolín Camarena <omar.antolin@gmail.com>
2024-09-25 14:43:54 -0600

    Add a triangulation of the torus

--- afcom.lisp
+++ afcom.lisp
@@ -271,3 +271,8 @@ pairs, those for which the triple should not be affinely commutative."
   '((1 2 3) (1 2 6) (1 3 4) (1 4 5) (1 5 6)
     (2 3 8) (2 7 8) (3 4 8) (4 5 8) (5 7 8)
     (5 6 7)))
+
+(defparameter torus ; small triangulation of the torus
+  '((1 2 4) (2 4 5) (4 5 7) (5 7 8) (1 7 8) (1 2 8) (2 3 5) (3 5 6)
+    (5 6 8) (6 8 9) (2 8 9) (2 3 9) (1 3 6) (1 4 6) (4 6 9) (4 7 9)
+    (3 7 9) (1 3 7)))
